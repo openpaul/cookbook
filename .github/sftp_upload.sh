e6a75e15744ea15a547eb4ade030256b81689f43
Paul Saary <github@paulsaary.de>
2023-09-15 22:47:11 +0200

    ci

--- .github/sftp_upload.sh
+++ .github/sftp_upload.sh
@@ -5,8 +5,6 @@ set password $env(FTP_PASSWORD)
 set server $env(SERVER)
 set user $env(FTP_USER)
 
-
-
 # Execute the SCP command with password handling
 spawn  bash -c "scp -r site/* '$user'@$server:"
 expect "Please type 'yes', 'no' or the fingerprint:"
