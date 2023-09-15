72672a68d4bb5d83b52028a0a291bdaefb9e24bc
Paul Saary <github@paulsaary.de>
2023-09-15 20:47:16 +0200

    lets start

---
+++ build.sh
@@ -0,0 +1,8 @@
+#sudo apt install hugo
+pyenv activate 
+pip install mkdocs\
+            mkdocs-material \
+            mkdocs-minify-plugin \
+            mkdocs-redirects \
+            mkdocs-git-committers-plugin-2 \
+            mkdocs-git-revision-date-localized-plugin
---
+++ docs/index.md
@@ -0,0 +1,7 @@
+# About
+
+This is a recipe collection written in cooklang.
+
+## Inspiration
+
+This cookbook was insipred by the very pretty [nicholaswilde.io/recipes](https://nicholaswilde.io/recipes/).
