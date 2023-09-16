696a41bf85752d2a997e27987394588e818c406f
Paul Saary <github@paulsaary.de>
2023-09-16 18:05:15 +0200

    autoconfirm ssh

--- .github/lftp.sh
+++ .github/lftp.sh
@@ -12,7 +12,7 @@ remote_host="$4"
 remote_folder="$5"
 
 
-SETTINGS="set net:timeout 2; set net:max-retries 2; set net:reconnect-interval-base 2; set ftp:ssl-force yes; set ftp:ssl-protect-data true;"
+SETTINGS="set net:timeout 2; set net:max-retries 2; set net:reconnect-interval-base 2; set ftp:ssl-force yes; set ftp:ssl-protect-data true; set sftp:auto-confirm yes; set ssl:verify-certificate no;"
 # Use lftp to synchronize the folders
 # https://stackoverflow.com/questions/49843692/continuous-deployment-using-lftp-gets-stuck-temporarily-after-about-10-files
 #lftp -c "set net:timeout 5; set net:max-retries 2; set net:reconnect-interval-base 5; set ftp:ssl-force yes; set ftp:ssl-protect-data true; open -u $USERNAME,$PASSWORD $HOST; mirror dist / -Renv --parallel=10"
