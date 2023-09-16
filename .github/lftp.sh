ad30e19275e6988086eafa1ec3af97f9a2d2cb0b
Paul Saary <github@paulsaary.de>
2023-09-16 18:08:38 +0200

    ci

--- .github/lftp.sh
+++ .github/lftp.sh
@@ -12,8 +12,8 @@ remote_host="$4"
 remote_folder="$5"
 
 
-SETTINGS="set net:timeout 2; set net:max-retries 2; set net:reconnect-interval-base 2; set ftp:ssl-force yes; set ftp:ssl-protect-data true; set sftp:auto-confirm yes; set ssl:verify-certificate no;"
+SETTINGS="set net:timeout 5; set net:max-retries 5; set net:reconnect-interval-base 5; set ftp:ssl-force yes; set ftp:ssl-protect-data true; set sftp:auto-confirm yes; set ssl:verify-certificate no;"
 # Use lftp to synchronize the folders
 # https://stackoverflow.com/questions/49843692/continuous-deployment-using-lftp-gets-stuck-temporarily-after-about-10-files
-#lftp -c "set net:timeout 5; set net:max-retries 2; set net:reconnect-interval-base 5; set ftp:ssl-force yes; set ftp:ssl-protect-data true; open -u $USERNAME,$PASSWORD $HOST; mirror dist / -Renv --parallel=10"
+# https://forum.gitlab.com/t/deploy-with-lftp-uploads-all-files-even-unchanged-ones/35439
 lftp -e "$SETTINGS; ls; mirror -P 5 -R --verbose \"$local_folder\" \"$remote_folder\"; quit" -u "$remote_user","$remote_password" sftp://$remote_host
