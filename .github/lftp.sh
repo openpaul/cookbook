863bc00684adb658d3bca8387204a2fd4be3427f
Paul Saary <github@paulsaary.de>
2023-09-17 10:49:14 +0200

    lets go

--- .github/lftp.sh
+++ .github/lftp.sh
@@ -17,7 +17,7 @@ SETTINGS="set net:timeout 2; set net:max-retries 2; set net:reconnect-interval-b
 # Use lftp to synchronize the folders
 # https://stackoverflow.com/questions/49843692/continuous-deployment-using-lftp-gets-stuck-temporarily-after-about-10-files
 # https://forum.gitlab.com/t/deploy-with-lftp-uploads-all-files-even-unchanged-ones/35439
-output=$(lftp -p 22 -e "$SETTINGS; ls; mirror -P 5 -R --verbose \"$local_folder\" \"$remote_folder\"; quit" -u "$remote_user","$remote_password" sftp://$remote_host 2>&1)
+output=$(lftp -p 22 -e "$SETTINGS; ls; mirror --delete -P 5 -R --verbose \"$local_folder\" \"$remote_folder\"; quit" -u "$remote_user","$remote_password" sftp://$remote_host 2>&1)
 exit_code=$?
 
 if [ $exit_code -eq 0 ]; then
