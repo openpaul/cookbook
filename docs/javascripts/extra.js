7c126db4ae7856ac7ba798b6ab5445a2d6488e37
Paul Saary <github@paulsaary.de>
2024-03-24 18:09:12 +0100

    check for support

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -12,18 +12,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
   let wakeLock = null;
 
-  
-
   const requestWakeLock = async () => {
-    try {
-      wakeLock = await navigator.wakeLock.request('screen');      
-      wakeLock.addEventListener('release', () => {
-        console.log('Wake Lock was released');
-      });
-      console.log('Wake Lock is active');
-    }
-    catch(err) {
-      console.error(`Caught ${err.name} acquiring screen lock: ${err.message}`);
+    if ('wakeLock' in navigator) {
+      try {
+        wakeLock = await navigator.wakeLock.request('screen');      
+        wakeLock.addEventListener('release', () => {
+          console.log('Wake Lock was released');
+        });
+        console.log('Wake Lock is active');
+      } catch(err) {
+        console.error(`Caught ${err.name} acquiring screen lock: ${err.message}`);
+      }
+    } else {
+      console.error('Wake Lock API is not supported.');
     }
   };
 
