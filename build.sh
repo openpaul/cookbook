82b52aeebea6a2b2218aee3b91539bce313026bd
Paul Saary <github@paulsaary.de>
2024-10-20 16:03:35 +0200

    include delete

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ pycook -i cook/ -o docs/
 rsync -av --ignore-existing --include "*/" --include="*.webp"  --exclude="*" cook/ docs/
 python .github/gallery.py cook >> docs/index.md
 mkdocs build -c
-rsync -za site $USER@ginger:/var/www/recipes/
+rsync -za --delete site $USER@ginger:/var/www/recipes/
 #mkdocs serve
 git restore docs
 rm -rf site
